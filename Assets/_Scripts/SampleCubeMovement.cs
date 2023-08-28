f03c449ee5acbad3f4dedbcbde175ef4170a4600
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-08-28 17:50:41 +0200

    Added the jumpForce and moveForce as public variables to the SampleCubeMovement script

--- Assets/_Scripts/SampleCubeMovement.cs
+++ Assets/_Scripts/SampleCubeMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class SampleCubeMovement : MonoBehaviour
 {
+    public float moveForce = 10f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb; //The rigidbody component is used to access the physics of the object
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); //Get the rigidbody component from the object
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
