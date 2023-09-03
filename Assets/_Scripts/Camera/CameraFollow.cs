76299c6895bced945cedbd7c9c293612047bd67b
Marek Kacprzak <kacprzakmarek92@gmail.com>
2023-09-03 13:19:13 +0200

    Adjusted the camera's zoom position

--- Assets/_Scripts/Camera/CameraFollow.cs
+++ Assets/_Scripts/Camera/CameraFollow.cs
@@ -6,18 +6,19 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target; // target for the camera to follow
 
-    private Vector3 offset; // offset of the camera
-
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - target.position; // set the offset to the difference between the camera and the target
+
     }
 
     // LateUpdate is called after Update, to ensure that the player's movement has already been processed
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset; // set the desired position to the target's position plus the offset
-        transform.position = desiredPosition; // set the camera's position to the smoothed position
+        if (target != null)
+        {
+            // set the camera's position to the player's position
+            transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        }
     }
 }
